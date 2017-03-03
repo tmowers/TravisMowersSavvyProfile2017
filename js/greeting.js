a07588994ae9bbf1a0b7b7b730ceda3192df539b
Travis Mowers <mowers@Traviss-MacBook-Air.local>
2017-03-02 20:55:34 -0600

    Create javascript

---
+++ js/greeting.js
@@ -0,0 +1,11 @@
+var nameTest = function( name ){
+
+if( !name ){
+alert( "please input a name" );
+}
+else if( name === "Travis"){
+  alert( "Welcome to my website.");
+}
+};
+
+nameTest( prompt( "What is your name?" ) );
